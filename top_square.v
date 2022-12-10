ff9b9f69397023ce39dc79e3370069526a376663
Jack Edelist <68572859+edelist@users.noreply.github.com>
2022-12-09 19:28:37 -0500

    Update top_square.v

--- top_square.v
+++ top_square.v
@@ -101,70 +101,70 @@ module top_square(
         M1_0, M1_1, M1_2, M1_3, M1_4, M1_5, M1_6, M2_0, M2_1, M2_2, M2_3, M2_4, M2_5, M2_6, 
         S1_0, S1_1, S1_2, S1_3, S1_4, S1_5, S1_6, S2_0, S2_1, S2_2, S2_3, S2_4, S2_5, S2_6; 
     wire colonHM_1, colonHM_2, colonMS_1, colonMS_2; //add ms functionality if necessary
-    wire A_P0, A_P1, A_P2, A_P3, A_P4, A_P5, A_P6;
+    wire A_P0, A_P1, A_P2, A_P3, A_P4, A_P5, A_P6;                                                     //0-3, 1-2, 4-5
     //HOUR 1 DIGIT
-    assign H1_0 = ((x > 75) & (y > 260) & (x < 105) & (y < 275)) ? 1 : 0;
-    assign H1_1 = ((x > 105) & (y > 230) & (x < 120) & (y < 260)) ? 1 : 0;
-    assign H1_2 = ((x > 105) & (y > 200) & (x < 120) & (y < 230)) ? 1 : 0; 
-    assign H1_3 = ((x > 75) & (y > 185) & (x < 105) & (y < 200)) ? 1 : 0; 
-    assign H1_4 = ((x > 60) & (y > 200) & (x < 75) & (y < 230)) ? 1 : 0; 
-    assign H1_5 = ((x > 60) & (y > 230) & (x < 75) & (y < 260)) ? 1 : 0;
+    assign H1_0 = ((x > 75) & (y > 185) & (x < 105) & (y < 200)) ? 1 : 0;
+    assign H1_1 = ((x > 105) & (y > 200) & (x < 120) & (y < 230)) ? 1 : 0;
+    assign H1_2 = ((x > 105) & (y > 230) & (x < 120) & (y < 260)) ? 1 : 0; 
+    assign H1_3 = ((x > 75) & (y > 260) & (x < 105) & (y < 275)) ? 1 : 0; 
+    assign H1_4 = ((x > 60) & (y > 230) & (x < 75) & (y < 260)) ? 1 : 0; 
+    assign H1_5 = ((x > 60) & (y > 200) & (x < 75) & (y < 230)) ? 1 : 0;
     assign H1_6 = ((x > 75) & (y > 222) & (x < 105) & (y < 238)) ? 1 : 0; //don't change y-value for #6, or for any of them really. numbers are stacked horizontally, not vertically
     //HOUR 2 DIGIT                                                                     
-    assign H2_0 = ((x > 150) & (y > 260) & (x < 180) & (y < 275)) ? 1 : 0;
-    assign H2_1 = ((x > 180) & (y > 230) & (x < 195) & (y < 260)) ? 1 : 0;
-    assign H2_2 = ((x > 180) & (y > 200) & (x < 195) & (y < 230)) ? 1 : 0;
-    assign H2_3 = ((x > 150) & (y > 185) & (x < 180) & (y < 200)) ? 1 : 0; //added 75 to H1 x-locations
-    assign H2_4 = ((x > 135) & (y > 200) & (x < 150) & (y < 230)) ? 1 : 0;
-    assign H2_5 = ((x > 135) & (y > 230) & (x < 150) & (y < 260)) ? 1 : 0;
+    assign H2_0 = ((x > 150) & (y > 185) & (x < 180) & (y < 200)) ? 1 : 0;
+    assign H2_1 = ((x > 180) & (y > 200) & (x < 195) & (y < 230)) ? 1 : 0;
+    assign H2_2 = ((x > 180) & (y > 230) & (x < 195) & (y < 260)) ? 1 : 0;
+    assign H2_3 = ((x > 150) & (y > 260) & (x < 180) & (y < 275)) ? 1 : 0; //added 75 to H1 x-locations
+    assign H2_4 = ((x > 135) & (y > 230) & (x < 150) & (y < 260)) ? 1 : 0;
+    assign H2_5 = ((x > 135) & (y > 200) & (x < 150) & (y < 230)) ? 1 : 0;
     assign H2_6 = ((x > 150) & (y > 222) & (x < 180) & (y < 238)) ? 1 : 0;
     //COLON HM DOTS
     assign colonHM_1 = ((x > 225) & (y > 200) & (x < 240) & (y < 215)) ? 1 : 0;
     assign colonHM_2 = ((x > 225) & (y > 245) & (x < 240) & (y < 260)) ? 1 : 0;
 
     //MINUTE 1 DIGIT
-    assign M1_0 = ((x > 285) & (y > 260) & (x < 315) & (y < 275)) ? 1 : 0;
-    assign M1_1 = ((x > 315) & (y > 230) & (x < 330) & (y < 260)) ? 1 : 0; //added 210 to H1 x-loc
-    assign M1_2 = ((x > 315) & (y > 200) & (x < 330) & (y < 230)) ? 1 : 0;
-    assign M1_3 = ((x > 285) & (y > 185) & (x < 315) & (y < 200)) ? 1 : 0;
-    assign M1_4 = ((x > 270) & (y > 200) & (x < 285) & (y < 230)) ? 1 : 0;
-    assign M1_5 = ((x > 270) & (y > 230) & (x < 285) & (y < 260)) ? 1 : 0;
+    assign M1_0 = ((x > 285) & (y > 185) & (x < 315) & (y < 200)) ? 1 : 0;
+    assign M1_1 = ((x > 315) & (y > 200) & (x < 330) & (y < 230)) ? 1 : 0; //added 210 to H1 x-loc
+    assign M1_2 = ((x > 315) & (y > 230) & (x < 330) & (y < 260)) ? 1 : 0;
+    assign M1_3 = ((x > 285) & (y > 260) & (x < 315) & (y < 275)) ? 1 : 0;
+    assign M1_4 = ((x > 270) & (y > 230) & (x < 285) & (y < 260)) ? 1 : 0;
+    assign M1_5 = ((x > 270) & (y > 200) & (x < 285) & (y < 230)) ? 1 : 0;
     assign M1_6 = ((x > 285) & (y > 222) & (x < 315) & (y < 238)) ? 1 : 0;  
     //MINUTE 2 DIGIT                                                                     
-    assign M2_0 = ((x > 360) & (y > 260) & (x < 390) & (y < 275)) ? 1 : 0;
-    assign M2_1 = ((x > 390) & (y > 230) & (x < 405) & (y < 260)) ? 1 : 0;//added 75 to M1 x-loc
-    assign M2_2 = ((x > 390) & (y > 200) & (x < 405) & (y < 230)) ? 1 : 0;
-    assign M2_3 = ((x > 360) & (y > 185) & (x < 390) & (y < 200)) ? 1 : 0;
-    assign M2_4 = ((x > 345) & (y > 200) & (x < 360) & (y < 230)) ? 1 : 0;
-    assign M2_5 = ((x > 345) & (y > 230) & (x < 360) & (y < 260)) ? 1 : 0;
+    assign M2_0 = ((x > 360) & (y > 185) & (x < 390) & (y < 200)) ? 1 : 0;
+    assign M2_1 = ((x > 390) & (y > 200) & (x < 405) & (y < 230)) ? 1 : 0;//added 75 to M1 x-loc
+    assign M2_2 = ((x > 390) & (y > 230) & (x < 405) & (y < 260)) ? 1 : 0;
+    assign M2_3 = ((x > 360) & (y > 260) & (x < 390) & (y < 275)) ? 1 : 0;
+    assign M2_4 = ((x > 345) & (y > 230) & (x < 360) & (y < 260)) ? 1 : 0;
+    assign M2_5 = ((x > 345) & (y > 200) & (x < 360) & (y < 230)) ? 1 : 0;
     assign M2_6 = ((x > 360) & (y > 222) & (x < 390) & (y < 238)) ? 1 : 0;
     //COLON MS DOTS
     assign colonMS_1 = ((x > 435) & (y > 200) & (x < 450) & (y < 215)) ? 1 : 0; //added 210 to CHM x-loc
     assign colonMS_2 = ((x > 435) & (y > 245) & (x < 450) & (y < 260)) ? 1 : 0;
     
     //SECOND 1 DIGIT
-    assign S1_0 = ((x > 495) & (y > 260) & (x < 525) & (y < 275)) ? 1 : 0;
-    assign S1_1 = ((x > 525) & (y > 230) & (x < 540) & (y < 260)) ? 1 : 0;
-    assign S1_2 = ((x > 525) & (y > 200) & (x < 540) & (y < 230)) ? 1 : 0;
-    assign S1_3 = ((x > 495) & (y > 185) & (x < 525) & (y < 200)) ? 1 : 0; //added 420 to H1 x-loc
-    assign S1_4 = ((x > 480) & (y > 200) & (x < 495) & (y < 230)) ? 1 : 0;
-    assign S1_5 = ((x > 480) & (y > 230) & (x < 495) & (y < 260)) ? 1 : 0;
+    assign S1_0 = ((x > 495) & (y > 185) & (x < 525) & (y < 200)) ? 1 : 0;
+    assign S1_1 = ((x > 525) & (y > 200) & (x < 540) & (y < 230)) ? 1 : 0;
+    assign S1_2 = ((x > 525) & (y > 230) & (x < 540) & (y < 260)) ? 1 : 0;
+    assign S1_3 = ((x > 495) & (y > 260) & (x < 525) & (y < 275)) ? 1 : 0; //added 420 to H1 x-loc
+    assign S1_4 = ((x > 480) & (y > 230) & (x < 495) & (y < 260)) ? 1 : 0;
+    assign S1_5 = ((x > 480) & (y > 200) & (x < 495) & (y < 230)) ? 1 : 0;
     assign S1_6 = ((x > 495) & (y > 222) & (x < 525) & (y < 238)) ? 1 : 0; 
     //SECOND 2 DIGIT                                                                     
-    assign S2_0 = ((x > 570) & (y > 260) & (x < 600) & (y < 275)) ? 1 : 0;
-    assign S2_1 = ((x > 600) & (y > 230) & (x < 615) & (y < 260)) ? 1 : 0;
-    assign S2_2 = ((x > 600) & (y > 200) & (x < 615) & (y < 230)) ? 1 : 0;
-    assign S2_3 = ((x > 570) & (y > 185) & (x < 600) & (y < 200)) ? 1 : 0; //added 75 to S1 x-locations
-    assign S2_4 = ((x > 555) & (y > 200) & (x < 570) & (y < 230)) ? 1 : 0;
-    assign S2_5 = ((x > 555) & (y > 230) & (x < 570) & (y < 260)) ? 1 : 0;
+    assign S2_0 = ((x > 570) & (y > 185) & (x < 600) & (y < 200)) ? 1 : 0;
+    assign S2_1 = ((x > 600) & (y > 200) & (x < 615) & (y < 230)) ? 1 : 0;
+    assign S2_2 = ((x > 600) & (y > 230) & (x < 615) & (y < 260)) ? 1 : 0;
+    assign S2_3 = ((x > 570) & (y > 260) & (x < 600) & (y < 275)) ? 1 : 0; //added 75 to S1 x-locations
+    assign S2_4 = ((x > 555) & (y > 230) & (x < 570) & (y < 260)) ? 1 : 0;
+    assign S2_5 = ((x > 555) & (y > 200) & (x < 570) & (y < 230)) ? 1 : 0;
     assign S2_6 = ((x > 570) & (y > 222) & (x < 600) & (y < 238)) ? 1 : 0;
 
-    assign A_P0 = ((x > 570) & (y > 160) & (x < 600) & (y < 175)) ? 1 : 0;
-    assign A_P1 = ((x > 600) & (y > 130) & (x < 615) & (y < 160)) ? 1 : 0;
-    assign A_P2 = ((x > 600) & (y > 100) & (x < 615) & (y < 130)) ? 1 : 0;
-    assign A_P3 = ((x > 570) & (y > 85) & (x < 600) & (y < 100)) ? 1 : 0; //added 75 to S1 x-locations
-    assign A_P4 = ((x > 555) & (y > 100) & (x < 570) & (y < 130)) ? 1 : 0;
-    assign A_P5 = ((x > 555) & (y > 130) & (x < 570) & (y < 160)) ? 1 : 0;
+    assign A_P0 = ((x > 570) & (y > 85) & (x < 600) & (y < 100)) ? 1 : 0;
+    assign A_P1 = ((x > 600) & (y > 100) & (x < 615) & (y < 130)) ? 1 : 0;
+    assign A_P2 = ((x > 600) & (y > 130) & (x < 615) & (y < 160)) ? 1 : 0;
+    assign A_P3 = ((x > 570) & (y > 160) & (x < 600) & (y < 175)) ? 1 : 0; //added 75 to S1 x-locations // not done btw
+    assign A_P4 = ((x > 555) & (y > 130) & (x < 570) & (y < 160)) ? 1 : 0;
+    assign A_P5 = ((x > 555) & (y > 100) & (x < 570) & (y < 130)) ? 1 : 0;
     assign A_P6 = ((x > 570) & (y > 122) & (x < 600) & (y < 138)) ? 1 : 0;
 
  // Assign the registers to the VGA 3rd output. This will display strong red on the Screen when 
@@ -180,151 +180,151 @@ module top_square(
     green = 0;
 	//This statement makes it so that within SQ1, a 3x3 grid of squares appears, with the middle square blacked out
     grid = SQ1 /*+ H1_0+H1_1+H1_2+H1_3+H1_4+H1_5+H1_6+H2_0+H2_1+H2_2+H2_3+H2_4+H2_5+H2_6+M1_0+M1_1+M1_2+M1_3+M1_4+M1_5+M1_6+M2_0+M2_1+M2_2+M2_3+M2_4+M2_5+M2_6+S1_0+S1_1+S1_2+S1_3+S1_4+S1_5+S1_6+S2_0+S2_1+S2_2+S2_3+S2_4+S2_5+S2_6*/+colonHM_1+colonHM_2+colonMS_1+colonMS_2;
-    if(h1[0] == 0) begin
-        grid = grid + H1_0;
+    if(h1[6] == 0) begin
+        grid = grid + H1_0;   //h1 = 1001111 --> h1[0]
     end
-    if(h1[1] == 0) begin
+    if(h1[5] == 0) begin
         grid = grid + H1_1;
     end
-    if(h1[2] == 0) begin
+    if(h1[4] == 0) begin
         grid = grid + H1_2;
     end
     if(h1[3] == 0) begin
         grid = grid + H1_3;
     end
-    if(h1[4] == 0) begin
+    if(h1[2] == 0) begin
         grid = grid + H1_4;
     end
-    if(h1[5] == 0) begin
+    if(h1[1] == 0) begin
         grid = grid + H1_5;
     end
-    if(h1[6] == 0) begin
+    if(h1[0] == 0) begin
         grid = grid + H1_6;
     end
-    if(h2[0] == 0) begin
+    if(h2[6] == 0) begin
         grid = grid + H2_0;
     end
-    if(h2[1] == 0) begin
+    if(h2[5] == 0) begin
         grid = grid + H2_1;
     end
-    if(h2[2] == 0) begin
+    if(h2[4] == 0) begin
         grid = grid + H2_2;
     end
     if(h2[3] == 0) begin
         grid = grid + H2_3;
     end
-    if(h2[4] == 0) begin
+    if(h2[2] == 0) begin
         grid = grid + H2_4;
     end
-    if(h2[5] == 0) begin
+    if(h2[1] == 0) begin
         grid = grid + H2_5;
     end
-    if(h2[6] == 0) begin
+    if(h2[0] == 0) begin
         grid = grid + H2_6;
     end
-    if(m1[0] == 0) begin
+    if(m1[6] == 0) begin
         grid = grid + M1_0;
     end
-    if(m1[1] == 0) begin
+    if(m1[5] == 0) begin
         grid = grid + M1_1;
     end
-    if(m1[2] == 0) begin
+    if(m1[4] == 0) begin
         grid = grid + M1_2;
     end
     if(m1[3] == 0) begin
         grid = grid + M1_3;
     end
-    if(m1[4] == 0) begin
+    if(m1[2] == 0) begin
         grid = grid + M1_4;
     end
-    if(m1[5] == 0) begin
+    if(m1[1] == 0) begin
         grid = grid + M1_5;
     end
-    if(m1[6] == 0) begin
+    if(m1[0] == 0) begin
         grid = grid + M1_6;
     end
-    if(m2[0] == 0) begin
+    if(m2[6] == 0) begin
         grid = grid + M2_0;
     end
-    if(m2[1] == 0) begin
+    if(m2[5] == 0) begin
         grid = grid + M2_1;
     end
-    if(m2[2] == 0) begin
+    if(m2[4] == 0) begin
         grid = grid + M2_2;
     end
     if(m2[3] == 0) begin
         grid = grid + M2_3;
     end
-    if(m2[4] == 0) begin
+    if(m2[2] == 0) begin
         grid = grid + M2_4;
     end
-    if(m2[5] == 0) begin
+    if(m2[1] == 0) begin
         grid = grid + M2_5;
     end
-    if(m2[6] == 0) begin
+    if(m2[0] == 0) begin
         grid = grid + M2_6;
     end
-    if(s1[0] == 0) begin
+    if(s1[6] == 0) begin
         grid = grid + S1_0;
     end
-    if(s1[1] == 0) begin
+    if(s1[5] == 0) begin
         grid = grid + S1_1;
     end
-    if(s1[2] == 0) begin
+    if(s1[4] == 0) begin
         grid = grid + S1_2;
     end
     if(s1[3] == 0) begin
         grid = grid + S1_3;
     end
-    if(s1[4] == 0) begin
+    if(s1[2] == 0) begin
         grid = grid + S1_4;
     end
-    if(s1[5] == 0) begin
+    if(s1[1] == 0) begin
         grid = grid + S1_5;
     end
-    if(s1[6] == 0) begin
+    if(s1[0] == 0) begin
         grid = grid + S1_6;
     end
-    if(s2[0] == 0) begin
+    if(s2[6] == 0) begin
         grid = grid + S2_0;
     end
-    if(s2[1] == 0) begin
+    if(s2[5] == 0) begin
         grid = grid + S2_1;
     end
-    if(s2[2] == 0) begin
+    if(s2[4] == 0) begin
         grid = grid + S2_2;
     end
     if(s2[3] == 0) begin
         grid = grid + S2_3;
     end
-    if(s2[4] == 0) begin
+    if(s2[2] == 0) begin
         grid = grid + S2_4;
     end
-    if(s2[5] == 0) begin
+    if(s2[1] == 0) begin
         grid = grid + S2_5;
     end
-    if(s2[6] == 0) begin
+    if(s2[0] == 0) begin
         grid = grid + S2_6;
     end
-    if(ap[0] == 0) begin
+    if(ap[6] == 0) begin
         grid = grid + A_P0;
     end
-    if(ap[1] == 0) begin
+    if(ap[5] == 0) begin
         grid = grid + A_P1;
     end
-    if(ap[2] == 0) begin
+    if(ap[4] == 0) begin
         grid = grid + A_P2;
     end
     if(ap[3] == 0) begin
         grid = grid + A_P3;
     end
-    if(ap[4] == 0) begin
+    if(ap[2] == 0) begin
         grid = grid + A_P4;
     end
-    if(ap[5] == 0) begin
+    if(ap[1] == 0) begin
         grid = grid + A_P5;
     end
-    if(ap[6] == 0) begin
+    if(ap[0] == 0) begin
         grid = grid + A_P6;
     end
 //    if(random_num[0] == 1)
